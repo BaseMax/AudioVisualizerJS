f0b1f521248d976f8147d67ce94614ef6ef2a077
Max Base <maxbasecode@gmail.com>
2022-08-01 14:35:21 +0200

    improve

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ const drawVisualiser = (bufferLength, dataArray) => {
         ctx.translate(elm_canvas.width / 2, elm_canvas.height / 2);
         ctx.rotate(i * 4.0001);
 
-        const hue = (Math.random() * 255) + i * 0.05;
-        ctx.fillStyle = 'hsl(' + hue + ',100%,50%)';
+        const hueColor = (Math.random() * 255) + i * 0.05;
+        ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
         ctx.beginPath();
         ctx.arc(10, barHeight / 2, barHeight / 2, 0, Math.PI / 4);
         ctx.fill();
@@ -39,8 +39,12 @@ const startVisualizer = () => {
 
         audioSource.connect(analyser);
         analyser.connect(audioContext.destination);
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/fftSize
+        // Must be a power of 2 between 2^5 and 2^15, so one of: 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, and 32768. Defaults to 2048.
         analyser.fftSize = 1024;
     }
+
     elm_audio.play();
     
     const bufferLength = analyser.frequencyBinCount;
@@ -52,22 +56,22 @@ const startVisualizer = () => {
         drawVisualiser(bufferLength, dataArray);
         requestAnimationFrame(animate);
     };
+
     animate();
 };
 
-// Evenets
-elm_container.addEventListener('click', startVisualizer);
+const init = () => {
+    elm_canvas.width = window.innerWidth;
+    elm_canvas.height = window.innerHeight;
+};
 
+// Evenets
 document.addEventListener('keydown', (e) => {
     if (e.keyCode === 32) startVisualizer();
 });
 
-window.addEventListener('load', () => {
-    elm_canvas.width = window.innerWidth;
-    elm_canvas.height = window.innerHeight;
-});
+window.addEventListener('load', init);
 
-window.addEventListener('resize', () => {
-    elm_canvas.width = window.innerWidth;
-    elm_canvas.height = window.innerHeight;
-});
+window.addEventListener('resize', init);
+
+elm_container.addEventListener('click', startVisualizer);
